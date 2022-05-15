de9cc033450c9ca5479bb2d2a42d23d4e7852416
Abi <awhittle@oregonstate.edu>
2022-05-14 22:06:36 -0700

    It doesnt have comments but Im tired and it works now :)

--- Todo List V2/app.js
+++ Todo List V2/app.js
@@ -2,6 +2,7 @@ const express = require("express"); // Allows for the use of express
 const bodyParser = require("body-parser"); // Allows for the use of body parser to read user input
 const ejs = require("ejs"); // Allows for the use of ejs
 const mongoose = require("mongoose"); // Allows for the use of the mongoose library
+const _ = require("lodash");
 
 const app = express(); // Express required
 app.use(bodyParser.urlencoded({ extended: true })); // Body parser required
@@ -62,28 +63,78 @@ app.get("/", function (req, res) {
 app.post("/", function (req, res) {
   // Post methods on the home route
   const itemName = req.body.newItem; // Creates a variable to hold the item created
+  const listName = req.body.list;
 
-  const newItem = new Item({ // Creates a new object from the Item model
-    name: itemName // Name is the text that the user typed into the form
+  const newItem = new Item({
+    // Creates a new object from the Item model
+    name: itemName, // Name is the text that the user typed into the form
   });
 
-  item.save(); // Adds to the collection
-
-  res.redirect("/"); // Goes back to the home route
+  if (listName === "Today") {
+    newItem.save(); // Adds to the collection
+    res.redirect("/"); // Goes back to the home route
+  } else {
+    List.findOne({ name: listName }, function (err, foundList) {
+      foundList.items.push(newItem);
+      foundList.save();
+      res.redirect("/" + listName);
+    });
+  }
 });
 
-app.post("/delete", function(req, res) { // Function to delete items from the database
+app.post("/delete", function (req, res) {
+  // Function to delete items from the database
+  const checkedItemId = req.body.checkbox; // Saves the value of the checkbox (which was the item id)
+  const listName = req.body.listName;
 
+  if (listName === "Today") {
+    Item.findByIdAndRemove(checkedItemId, function (err) {
+      // Function to find an item by its id and then remove it
+      if (!err) {
+        // If no errors happen
+        console.log("Successfully deleted item"); // Say that it was successful
+        res.redirect("/"); // Redirect to the home route to show the change
+      }
+    });
+  } else {
+    List.findOneAndUpdate(
+      { name: listName },
+      { $pull: { items: { _id: checkedItemId } } },
+      function (err, foundList) {
+        if (!err) {
+          res.redirect("/" + listName);
+        }
+      }
+    );
+  }
 });
 
-app.get("/work", function (req, res) {
-  // Creates the work route
-  res.render("list", { day: "Work List", items: workItems }); // Uses ejs to render the work route page
-});
+const listSchema = {
+  name: String,
+  items: [itemsSchema],
+};
 
-app.get("/about", function (req, res) {
-  // Creates the about route
-  res.render("about"); // Uses ejs to render the about route page
+const List = mongoose.model("List", listSchema);
+
+app.get("/:listName", function (req, res) {
+  const listName = _.capitalize(req.params.listName);
+
+  List.findOne({ name: listName }, function (err, foundList) {
+    if (!err) {
+      if (!foundList) {
+        const list = new List({
+          name: listName,
+          items: defaultItems,
+        });
+
+        list.save();
+
+        res.redirect("/" + listName);
+      } else {
+        res.render("list", { day: foundList.name, items: foundList.items });
+      }
+    }
+  });
 });
 
 app.listen(3000, function () {
